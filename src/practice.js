a1592ad844c5cb740cb0d1a5f2f0db78def97848
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-06-27 20:06:59 -0700

    getting odd error

---
+++ src/practice.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: 'process.env.DB_URL',
+})
+
+// const q1 = knexInstance('amazong_products').select('*').toQuery()
+//  const q2 = knexInstance.from('amazong_products').select('*').toQuery()
+
+//  console.log('q1:', q1)
+
+//  console.log('q2:', q2)
+
+
+knexInstance.from('amazong_products').select('*')
+  .then(result => {
+    console.log(result)
+  })
